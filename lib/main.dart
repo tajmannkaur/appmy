b9ac9b9ec8b23e372dc0fbe6dcd1b13d46481edd
tajnadha1317 <tajnadha1317@gmail.com>
2025-04-03 20:49:23 +0000

    update

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../screens/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  //Initialize Flutter bindings
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Initialize Firebase with platform's default options
+  await Firebase.initializeApp();
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
